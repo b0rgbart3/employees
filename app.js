ed00989ad6bc1bdb2d79fe808ff20e24a764378c
Bart Dority <b0rgBart3@gmail.com>
2020-07-22 08:41:58 -0700

    successfully adding and removing employees

--- app.js
+++ app.js
@@ -18,46 +18,51 @@ var employee;
 
 connection.connect(function(err) {
   if (err) throw err;
- // console.log("connected as id " + connection.threadId);
- // employee = new Employee(connection);
   mainMenu();
 });
 
 function mainMenu() {
-    // console.log("done.");
-  
- // var consoleView = new Viewing(connection);
+
   // Create an array of questions for Inquirer
   let questions = [ {type: 'list',
       message: 'What would you like to do next?',
       name: 'user_choice',
       choices:[ { name: 'View all employees'},
+                { name: 'View all employees by department'},
                 { name: 'Add an employee'},
-                { name: 'Add an role'},
-                { name: 'Add an department'}]}];
+                { name: 'Remove an employee'},
+                { name: 'Add a role'},
+                { name: 'Add a department'}]}];
 
   // invoke the Inquirer 
   inquirer
   .prompt(questions).then(answers => {
     switch(answers.user_choice) {
       case "View all employees":
-        //consoleView.allEmployees(connection);
         displayAll();
         break;
+      case "View all employees by department":
+        break;
       case "Add an employee":
-        gatherNewEmployee();
+        addNewEmployee();
+        break;
+      case "Remove an employee":
+        removeEmployee();
         break;
       case "Add a role":
         break;
       case "Add a department":
+        addNewDepartment();
         break;
     }
   });
 } // end of mainMenu function
 
 function displayAll() {
+  // This query pulls data from 3 different tables to 
+  // create a full chart of info for each employee
   var fullyJoinedDBQuery = 
-  `SELECT employee.first_name, employee.last_name AS e1, 
+  `SELECT employee.id, employee.first_name, employee.last_name, 
   role.title, department.name, role.salary,
   CONCAT( mgr.first_name, " ", mgr.last_Name) AS manager
   FROM employee
@@ -65,33 +70,38 @@ function displayAll() {
   INNER JOIN department ON role.department_id = department.id
   LEFT JOIN employee mgr ON employee.manager_id= mgr.id;`;
 
-  //"SELECT * FROM employee"
-
   connection.query(fullyJoinedDBQuery, function(err, res) {
   if (err) throw err;
 
+    // use console.table to generate a well formatted chart of data
     var myTable = tabler.getTable(res);
-    myOutput = myTable.split("null").join("");
+    
+    // take out the "nulls"
+    myOutput = myTable; // .split("null").join("");
 
+    // this console.log() is actually part of the app functionality
     console.log(myOutput);
     mainMenu();
   });
 }
-function gatherNewEmployee() {
+function addNewEmployee() {
 
+  // start by getting a list of roles from the db
   var role_query = `SELECT * FROM role;`;
 
   connection.query(role_query, function(err, roles) {
       if (err) throw err;
       let roleNames = roles.map(role=>{ return role.title; });
 
+      // get a full list of employee names so we can choose who the new employee's manager is by name
       let employee_query = `SELECT * FROM employee;`;
       connection.query(employee_query, function(err, employees) {
         if (err) throw err;
         
         let employeeNames = employees.map(employee=> { return (employee.first_name + " " + employee.last_name) ; });
       
-        var questions = [
+        // questions to ask for entering a new employee into the db
+        var newEmployeeQuestions = [
           {
               name: "first_name",
               message: "First Name:",
@@ -106,38 +116,39 @@ function gatherNewEmployee() {
               name: "role",
               message: "Role:",
               type: "list",
-              choices: roleNames,
+              choices: roleNames,  // this array is coming from the role_query
               default: "",
           },
           {
               name: "manager",
               message: "Manager:",
               type: "list",
-              choices: employeeNames,
+              choices: employeeNames, // this array is coming from the employee_query
               default: ""
           }];
     // invoke the Inquirer 
     inquirer
-    .prompt(questions).then(answers => {
+    .prompt(newEmployeeQuestions).then(answers => {
 
-    
+        // The user chose the role by name, but we need to store the role_id in the employee table
         let roleIndex = roleNames.indexOf(answers.role);
         let role_id = roles[roleIndex].id;
 
+        // The user chose the manager by name, but we need to store the manager_id in the employee table
         let managerIndex = employeeNames.indexOf(answers.manager);
         let manager_id = employees[managerIndex].id;
 
-        let newEmployee = { first_name: answers.first_name,
-                            last_name: answers.last_name,
-                            role_id: role_id,
-                            manager_id: manager_id};
-        console.log(newEmployee);
+        // let newEmployee = { first_name: answers.first_name,
+        //                     last_name: answers.last_name,
+        //                     role_id: role_id,
+        //                     manager_id: manager_id};
+        //console.log(newEmployee);
         let new_employee_query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES 
         ('${answers.first_name}', '${answers.last_name}', '${role_id}', '${manager_id}');`;
         connection.query(new_employee_query, function(err, newEmployee) {
-          if (err) throw err;
-          console.log("successfully added: "+ newEmployee);
-        mainMenu();
+            if (err) throw err;
+               console.log("successfully added: "+ newEmployee);
+            mainMenu();
         });
       });
 
@@ -145,7 +156,66 @@ function gatherNewEmployee() {
       
 });
 
-} // end of gather new employee
+} // end of add new employee
+
+
+function addNewDepartment() {
+     // questions to ask for entering a new employee into the db
+     var newEmployeeQuestions = [
+      {
+          name: "name",
+          message: "Department Name:",
+          default: "",
+      }];
+      // invoke the Inquirer 
+      inquirer
+      .prompt(newEmployeeQuestions).then(answers => {
+       // console.log(answers);
+        // Need some validation here to check for duplicates and/or bad data
+
+        new_dept_query = `INSERT INTO department (name) VALUES ('${answers.name}');`;
+        connection.query(new_dept_query, function(err, newDept) {
+            if (err) throw err;
+               console.log("successfully added: "+ answers.name);
+            mainMenu();
+        });
+
+      });
+} // end of add new department
+
+function removeEmployee() {
+    // get a full list of employee names so we can choose who the new employee's manager is by name
+    let employee_query = `SELECT * FROM employee;`;
+    connection.query(employee_query, function(err, employees) {
+      if (err) throw err;
+      
+      let employeeNames = employees.map(employee=> { return (employee.first_name + " " + employee.last_name) ; });
+      
+      let removeEmployeeQuestions = [
+        {
+            name: "name",
+            type: "list",
+            message: "Which employee do you need to remove?",
+            choices: employeeNames
+        }];
+        // invoke the Inquirer 
+        inquirer
+        .prompt(removeEmployeeQuestions).then(answers => {
+                 // The user chose the manager by name, but we need to store the manager_id in the employee table
+          let employeeIndex = employeeNames.indexOf(answers.name);
+          let employee_id = employees[employeeIndex].id;
+          let remove_query = `REMOVE * FROM employee WHERE ID='{$employee_id};`;
+          connection.query(employee_query, function(err, employee) {
+            if (err) throw err;
+            console.log("Successfully removed employee: " + answers.name);
+            mainMenu();
+          });
+
+        
+        });
+      });
+
+}
 
   // Add departments, roles, employees
   // View departments, roles, employees
